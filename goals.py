c6fe4b6b04352ddde12467316e616533c68e4d81
ZakB404 <complexx15z@gmail.com>
2026-01-09 15:41:14 +0000

    goals.py
    The future.

---
+++ goals.py
@@ -0,0 +1,7 @@
+# My AI Career Path
+name = "Future AI Engineer"
+target_company = "Amazon"
+dream_car = "Tesla Model 3"
+
+print(f"My name is Zak and I am working toward a degree apprenticeship at Amazon.")
+print(f"I'm learning Python now so I can afford a BMW M4 COMP by 2030!")
